4511045ef508837e8e0381c2818094493ea7a1f0
Thomas Craig <thomas.craig@liverpool.ac.uk>
2016-11-21 09:34:31 +0000

    Improve layout and functionality of orders module

--- app/modules/orders/main.js
+++ app/modules/orders/main.js
@@ -7,36 +7,64 @@ app.controller('OrdersCtrl', function($scope, PageTitle, OrderService) {
     PageTitle.set('Orders');
     $scope.removePadding = true;
 
-    $scope.orders = OrderService.orders();
-    OrderService.orders().then(function(data) {
-        $scope.orders = data;
+    $scope.selected = [];
+
+    var refreshData = function() {
+        OrderService.orders().then(function(data) {
+            $scope.orders = data;
+        });
+    };
+    refreshData();
+
+    $scope.$watch('query.search', function(n, o) {
+        if (n !== o) {
+            refreshData();
+        }
     });
+
+    $scope.onSortItems = function(order) {
+        refreshData();
+    };
+
+    $scope.onPaginateItems = function(page, limit) {
+        $scope.query.page = page;
+        $scope.query.limit = limit;
+        refreshData();
+    };
+
+    $scope.deleteItems = function(selected) {
+        var d = $mdDialog.confirm()
+            .title('Delete ' + selected.length + ' orders?')
+            .ok('Delete')
+            .cancel('No');
+        $mdDialog.show(d).then(function() {
+            var promises = [];
+            _.each(selected, function(obj) {
+                var p = OrderService.deleteOrder(obj.id);
+                promises.push(p);
+            });
+            $q.all(promises).then(function(data) {
+                refreshData();
+            });
+        });
+    };
 
 });
 
 app.controller('OrderDetailsCtrl', function($scope, PageTitle,
     OrderService, $stateParams, $state) {
 
-    $scope.removePadding = true;
     OrderService.order_details($stateParams.id).then(function(data) {
         $scope.order = data;
         PageTitle.set('Order #' + $scope.order.id + ': ' + $scope.order.name);
-
-        var services = {};
-        _.each($scope.order.data.samples, function(obj) {
-            services[obj.name] = obj.services;
-        });
-        $scope.total = _.reduce(services, function(total, n) {
-            return total + _.reduce(n, function(subtotal, m) {
-                return subtotal + m.cost;
-            }, 0);
-        }, 0);
-
     });
 
-    OrderService.statusBarOptions().then(function(data) {
-        $scope.order_bar_statuses = data;
-    });
+    $scope.isList = function(item) {
+        if (Array.isArray(item) || typeof item === 'object') {
+            return true;
+        }
+        return false;
+    };
 
     $scope.delete = function() {
         OrderService.remove($stateParams.id).then(function() {
@@ -44,6 +72,37 @@ app.controller('OrderDetailsCtrl', function($scope, PageTitle,
         });
     };
 
+});
+
+app.directive('gtlSubValue', function($sce, $compile) {
+    return {
+        restrict: 'A',
+        scope: {
+            data: '=',
+        },
+        link: function($scope, elem, attr) {
+            var output = '';
+
+            var processObject = function(obj) {
+                var out = '';
+                _.each(obj, function(value, key) {
+                    out += '<div layout="row"><b flex="25">'+ key + '</b><span flex class="order-item-value">' + value + '</span></div>';
+                });
+                return out;
+            };
+
+            if (Array.isArray($scope.data)) {
+                for (var i = 0; i < $scope.data.length; i++) {
+                    output += '<div class="subvalue-group">' + processObject($scope.data[i]) + '</div>';
+                }
+            } else {
+                output += processObject($scope.data);
+            }
+
+            var compiled = $compile(output)($scope);
+            elem.append(compiled);
+        }
+    }
 });
 
 app.service('OrderService', function(Restangular) {
