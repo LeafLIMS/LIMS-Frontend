885a33ba1265f0c2425dbec2e826571121b4fe78
Thomas Craig <thomas.craig@liverpool.ac.uk>
2017-12-13 16:06:49 +0000

    Add DOM monitoring to update selected on new item additions

--- src/components/semantic-ui/ui-dropdown.js
+++ src/components/semantic-ui/ui-dropdown.js
@@ -8,17 +8,35 @@ export class UiDropdownCustomElement {
 
     constructor(element) {
         this.element = element;
+
+        // observe the addition/removal of nodes from menu and
+        // update selection (to include new items etc.)
+        this.observer = new MutationObserver(m => {
+            if (this.dropdown) {
+                this.dropdown.dropdown('refresh');
+                this.dropdown.dropdown('set selected', this.value);
+            }
+        });
     }
 
     attached() {
+        let config = {childList: true, subList: true};
         this.dropdown = jQuery('.ui.selection.dropdown', this.element);
         this.dropdown.dropdown();
+
+        let observable = this.element.getElementsByClassName('menu');
+        this.observer.observe(observable[0], config);
+
         // The set up gets called before the elements are in place thus
         // not actually selecting them. This prevents that from happening.
         setTimeout(() => {
             //this.dropdown.dropdown('refresh');
             this.valueChanged(this.value);
         }, 1);
+    }
+
+    detached() {
+        this.observer.disconnect();
     }
 
     valueChanged(n) {
